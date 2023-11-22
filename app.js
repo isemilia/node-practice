b428374d55694c2bc2bdb3186a61a930d458ec14
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-11-22 07:06:04 +0600

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// set uup .env
+dotenv.config();
 
 // initialize express 
 const app = express();
 
+// connect to mongoDB
+mongoose.connect(process.env.DB_URI)
+    .then((res) => {
+        console.log('Connected to DB');
+
+        // listen for requests
+        app.listen(4000, () => {
+            console.log('Listening');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
 // register view engine
 app.set('view engine', 'ejs');
 app.set('views', 'pages');
-
-// listen for requests
-app.listen(4000, () => {
-    console.log('Listening');
-});
 
 // styles
 app.use(express.static(__dirname + '/public'));
