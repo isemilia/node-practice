7cb221500ba7759c66462201183eb7e2fe9c7de8
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-11-25 16:23:45 +0600

    Render posts from database

--- app.js
+++ app.js
@@ -35,63 +35,28 @@ app.use(express.static(__dirname + '/public'));
 // logger 
 app.use(morgan('dev'));
 
-// mongodb
-app.get('/create-post', (req, res) => {
-    const post = new Post({
-        title: 'Learning NodeJS',
-        snippet: 'NodeJS, Express, MongoDB',
-        body: 'Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more.'
-    });
-
-    // save post instance to database
-    post.save()
-        .then(result => {
-            res.send(result);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/all-posts', (req, res) => {
-    // get all posts
-    Post.find()
-        .then(result => {
-            res.send(result);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/single-post', (req, res) => {
-    Post.findById('6561bcaa0370884a478d3263')
-        .then(result => {
-            res.send(result);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
-
 // router
 app.get('/', (req, res) => {
-    const posts = [
-        { title: 'NodeJS', snippet: 'Lorem ipus liber metus' },
-        { title: 'JavaScript', snippet: 'Lorem ipus liber metus' },
-        { title: 'TypeScript', snippet: 'Lorem ipus liber metus' },
-    ]
-    res.render('index', { title: 'Home', posts });
+    res.redirect('/posts');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
+
+// post routes
+app.get('/posts', (req, res) => {
+    Post.find().sort({ createdAt: -1 })
+        .then(posts => {
+            res.render('index', { title: 'Posts', posts });
+        })
+        .catch(console.log)
+});
+
 app.get('/posts/create', (req, res) => {
     res.render('create', { title: 'Create a new post' });
-})
-
+});
 
 // redirects
 app.get('/about-us', (req, res) => {
