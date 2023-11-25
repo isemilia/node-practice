a184dc0692cc8feb26978fbc334bec186a6212ea
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-11-25 15:55:24 +0600

    Test database requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const Post = require('./models/post')
+
 // set uup .env
 dotenv.config();
 
@@ -33,6 +35,46 @@ app.use(express.static(__dirname + '/public'));
 // logger 
 app.use(morgan('dev'));
 
+// mongodb
+app.get('/create-post', (req, res) => {
+    const post = new Post({
+        title: 'Learning NodeJS',
+        snippet: 'NodeJS, Express, MongoDB',
+        body: 'Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more.'
+    });
+
+    // save post instance to database
+    post.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.get('/all-posts', (req, res) => {
+    // get all posts
+    Post.find()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.get('/single-post', (req, res) => {
+    Post.findById('6561bcaa0370884a478d3263')
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+})
+
+// router
 app.get('/', (req, res) => {
     const posts = [
         { title: 'NodeJS', snippet: 'Lorem ipus liber metus' },
